36a306b65853dc650dfbc5be0e65dae78cb14ec3
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-05-19 22:29:16 +0300

    task 4

--- src/helpers/formatToDate.js
+++ src/helpers/formatToDate.js
@@ -1,4 +1,8 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 export function formatToDate(date) {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
+}
+
+export function formatToPpDate(date) {
+  return format(new Date(date), 'Pp', { addSuffix: true });
 }
