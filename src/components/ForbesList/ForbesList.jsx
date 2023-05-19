54e31125e5a3dccee9ffe28778c44c8f7189c1cb
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-05-19 21:04:29 +0200

    tasks-3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,19 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({items}) => {
+  return <LeaderBoard>
+  <BoardHeader>
+    <BoardTitle>
+      <TitleTop>Forbes</TitleTop>
+      <TitleBottom>Leader board</TitleBottom>
+    </BoardTitle>
+  </BoardHeader>
+
+  <LeaderBoardProfiles>
+    {items.map(({name, id, capital, isIncrease, avatar}) =>(
+      <ForbesListItem key={id}  name= {name} capital = {capital} avatar = {avatar} isIncrease = {isIncrease}/>
+    ))}
+  </LeaderBoardProfiles>
+</LeaderBoard>;
 };
